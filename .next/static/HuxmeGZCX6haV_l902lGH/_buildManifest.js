013c8731f9a90f69e6feb9fdc4d8abefc7f0cd94
thomas-backens <thomas.backens@yahoo.com>
2022-12-06 14:37:09 -0600

    removed how

--- .next/static/HuxmeGZCX6haV_l902lGH/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-35bb597b260f35ab.js"],"/How":["static/chunks/pages/How-b40444e397db2c20.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/How","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
